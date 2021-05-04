df8dace7774fadfefa8ef1799f6f4ea275b0245c
celine.roure <celine.roure.pro@gmail.com>
2021-05-04 12:10:09 +1000

    implements radom loop

--- index.rb
+++ index.rb
@@ -1,6 +1,7 @@
-group = []
+group = ["alice", "bob", "charlie", "Dylan"]
 
 while true 
+  puts "There are #{group.length} members in that group"
   puts "press 1 to add a member to the group"
   puts "Press 2 display a random group"
   puts "Press 3 to quit"
@@ -15,10 +16,26 @@ while true
     puts "Enter name:"
     name = gets.chomp
     group.push(name)
+    pp group
 
   when 2
     puts "Random group output:"
-    #TODO - Output random group
+    random_order_loop_running = true
+    while random_order_loop_running
+      copied_group = group.clone
+      while copied_group.length > 0 
+        random_index = rand(copied_group.length - 1)
+        puts copied_group[random_index].capitalize
+        copied_group.delete_at(random_index)
+      end
+      puts "Press 1 to repeat"
+      puts "Press 2 to quit"
+      pp copied_group
+      choice = gets.chomp.to_i
+      if choice == 2
+        exit
+      end
+    end
 
   when 3
     #TODO - Check they want to quit
