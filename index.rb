421d504732738ad9965993fce38f4b67c88a1d08
celine.roure <celine.roure.pro@gmail.com>
2021-05-04 11:46:09 +1000

    modify the file  and fix some bugs

--- index.rb
+++ index.rb
@@ -3,21 +3,32 @@ group = []
 while true 
   puts "press 1 to add a member to the group"
   puts "Press 2 display a random group"
-  puts "Press3 to quit"
+  puts "Press 3 to quit"
 
     #take input from user
-  menu_input = gets.chomp
+  menu_input = gets.chomp.to_i
+
 
   case menu_input
+
   when 1
     puts "Enter name:"
-    name = gets.chompgroup.push(name)
+    name = gets.chomp
+    group.push(name)
+
   when 2
     puts "Random group output:"
     #TODO - Output random group
+
   when 3
     #TODO - Check they want to quit
     break
+
   else
     puts "Invalid input, please try again"
+  end
+  sleep(1)
+  system "clear"
 end
+
+puts "Goodbye, Thanks for visiting"
